a9770da13d86cd0b6eed4194b40a65761c1542e0
Polly Liu <polly.liu4@gmail.com>
2024-01-13 14:09:00 -0500

    set up spotify api

--- server/app.py
+++ server/app.py
@@ -1,6 +1,19 @@
+import os # spotify API
+import requests
 from flask import Flask
 
 app = Flask(__name__)
+
+# Spotify API setup
+spotify_api_key = spotify_api_key = os.getenv('SPOTIFY_API_KEY')
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+REDIRECT_URL = 'http://localhost:5000/callback'
+
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+API_BASE_URL = 'https://api.spotify.com/v1'
+
 
 @app.route('/')
 def home():
